96bc07cbc234ca6842e07690d08f6a4989d98e69
Jared Rummler <jared@jaredrummler.com>
2015-12-01 02:47:35 -0800

    getDeviceName method will now handle model and codename.

--- android-devices/src/main/java/com/jaredrummler/android/devices/Main.java
+++ android-devices/src/main/java/com/jaredrummler/android/devices/Main.java
@@ -21,52 +21,50 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class Main {
 
   // https://support.google.com/googleplay/answer/1727131?hl=en
   private static final String LATEST_DEVICE_LIST_XLS = "supported_devices_11-30-2015.xls";
 
-  public static void main(String[] args) throws IOException {
-    createJsonManifests();
+  public static void main(String[] args) throws Exception {
+    Main main = new Main();
+    List<Device> devices = main.getLatestDevices();
+    //main.createJsonFiles(devices);
+    DevicesToJava.printGetDeviceNameMethod(devices);
   }
 
-  private static void createJsonManifests() throws IOException {
-    InputStream inputStream = ClassLoader.getSystemResourceAsStream(LATEST_DEVICE_LIST_XLS);
-    DevicesParser parser = new DevicesParser();
-    List<Device> devices = parser.getDevices(inputStream);
+  private final Gson GSON = new Gson();
+
+  private final Type DEVICE_TYPE = new TypeToken<List<Device>>() {
+
+  }.getType();
+
+  public List<Device> getDevicesFromJson(File file) throws FileNotFoundException {
+    return GSON.fromJson(new JsonReader(new FileReader(file)), DEVICE_TYPE);
+  }
+
+  public List<Device> getDeviceFromXls(InputStream inputStream) throws IOException {
+    return new DevicesParser().getDevices(inputStream);
+  }
+
+  public List<Device> getLatestDevices() throws IOException {
+    return getDeviceFromXls(ClassLoader.getSystemResourceAsStream(LATEST_DEVICE_LIST_XLS));
+  }
+
+  public void createJsonFiles(List<Device> devices) throws IOException {
     DevicesToJson devicesToJson = new DevicesToJson(devices);
     devicesToJson.createDevicesJson(Constants.DEVICES_JSON);
     devicesToJson.createCodenamesJson(Constants.CODENAMES_DIR);
     devicesToJson.createManufacturersJson(Constants.MANUFACTURERS_DIR);
     devicesToJson.createPopularDevicesJson(Constants.POPULAR_DEVICES_JSON);
-    DevicesToJava.printMethod(devices);
-  }
-
-  // print new devices before creating the new JSON files.
-  private static void printNewDevices() throws IOException {
-    InputStream inputStream = ClassLoader.getSystemResourceAsStream(LATEST_DEVICE_LIST_XLS);
-    DevicesParser parser = new DevicesParser();
-    List<Device> newDeviceList = parser.getDevices(inputStream);
-    final Type DEVICE_TYPE = new TypeToken<List<Device>>() {
-
-    }.getType();
-    Gson gson = new Gson();
-    JsonReader reader = new JsonReader(new FileReader("json/devices.json"));
-    List<Device> oldDeviceList = gson.fromJson(reader, DEVICE_TYPE);
-    Set<String> devices =
-        newDeviceList.stream().filter(device -> !oldDeviceList.contains(device)).map(
-            device -> device.marketName + " : " + device.manufacturer)
-            .collect(Collectors.toCollection(TreeSet::new));
-    devices.forEach(System.out::println);
   }
 
 }
